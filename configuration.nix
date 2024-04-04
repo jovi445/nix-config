d6cf20fc0f1a27f3a972df956af296a2d62e6ba8
Jovi445 <75222136+jovi445@users.noreply.github.com>
2024-04-04 10:43:55 -0300

    Atualizar o configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "horologium"; # Define your hostname.
+  networking.hostName = "kazuza"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
